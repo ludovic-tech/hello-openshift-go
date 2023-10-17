3ab803e37b7ce3485231f14dc9dbde0fad8c5a4b
ludovic.tech <it.quenechdu@gmail.com>
2023-10-17 10:33:26 +0200

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Bienvenue fDeployements V2ormation Openshift "
+		response = "Bienvenue formation Openshift "
 	}
 
 	fmt.Fprintln(w, response)
